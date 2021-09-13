af8d5a595cd4712e10b8b1c6df7578e00787cf0d
오성식 <oss0202@naver.com>
2021-09-13 15:32:19 +0900

    JUnit5 LifeCycle Test

--- junit5-jupiter-starter-gradle/src/test/java/com/example/project/CalculatorTests.java
+++ junit5-jupiter-starter-gradle/src/test/java/com/example/project/CalculatorTests.java
@@ -10,32 +10,64 @@
 
 package com.example.project;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+//@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class CalculatorTests {
 
-	@Test
-	@DisplayName("1 + 1 = 2")
+	@BeforeAll
+	static void beforeAll(){
+		System.out.println("call BeforeALl");
+	}
+	@BeforeEach
+	void beforeEach(){
+		System.out.println("	call BeforeEach");
+	}
+
+	@Test//Test Method
+	@Order(1)
+	@DisplayName("1 + 1 = 2")//IDE에 표시될 이름
 	void addsTwoNumbers() {
+		System.out.println("		call Test addsTwoNumbers");
 		Calculator calculator = new Calculator();
 		assertEquals(2, calculator.add(1, 1), "1 + 1 should equal 2");
 	}
 
-	@ParameterizedTest(name = "{0} + {1} = {2}")
+	@Test//Test Method
+	@Order(2)
+	@DisplayName("10 + 10 = 22")//IDE에 표시될 이름
+	void addsTwoNumbers2() {
+		System.out.println("		call Test addsTwoNumbers222222");
+		Calculator calculator = new Calculator();
+		assertEquals(20, calculator.add(10, 10), "10 + 10 should equal 20");
+	}
+
+	@ParameterizedTest(name = "{0} + {1} = {2}")//매개변수화된 테스트
+	@Order(3)
 	@CsvSource({
 			"0,    1,   1",
 			"1,    2,   3",
 			"49,  51, 100",
 			"1,  100, 101"
-	})
+	})//인수 목록을 쉼표로 구분된 값(즉, String 리터럴-변수의 값이 변하지 않는 데이터, 메모리 위치 안의 값)
 	void add(int first, int second, int expectedResult) {
+		System.out.println("			call add method");
 		Calculator calculator = new Calculator();
 		assertEquals(expectedResult, calculator.add(first, second),
 				() -> first + " + " + second + " should equal " + expectedResult);
+	}
+
+	@AfterEach
+	void afterEach(){
+		System.out.println("	call AfterEach");
+	}
+	@AfterAll
+	static void afterAll(){
+		System.out.println("call AfterAll");
 	}
 }
